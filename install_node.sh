fbce0ec97f61d4885a4fe800a84aa3fb15c39b3a
webzhao <webzhao@gmail.com>
2015-11-26 18:29:00 +0800

    add node install

---
+++ install_node.sh
@@ -0,0 +1,8 @@
+wget https://nodejs.org/dist/v4.2.2/node-v4.2.2-linux-x64.tar.xz --no-check-certificate
+tar -xvf node-v4.2.2-linux-x64.tar.xz
+cd node-v4.2.2-linux-x64
+rm -rf README.md LICENSE CHANGELOG.md
+sudo cp -r * /usr/local/
+cd ..
+rm -rf node-v4.2.2-linux-x64
+rm -rf install_node.sh
